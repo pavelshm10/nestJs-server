06532c61c035a7e3a7d61a10ce2eb2ceec895773
pashShmil <pavels@realcemmerce.co.il>
2024-06-20 00:28:33 +0300

    ss

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 // Define routes
 app.get('/api/data', (req, res) => {
     // Handle the request, e.g., fetch data from a database
-    console.log("test")
+    console.log("test234")
     res.json({ message: 'Data from the server' });
 });
   
