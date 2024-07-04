23e52e67aeba6bdf574cb1110184adbe6b334937
pavelshm10 <pavelsh00@gmail.com>
2024-07-04 22:43:27 +0300

    upup

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const productRoutes = require('./routes/productRoutes');
 const mongoose = require('./config/dbConfig');
-
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use('/api', productRoutes);
 
 const PORT = process.env.PORT || 3000;
