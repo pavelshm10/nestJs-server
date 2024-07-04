ee7f523c988020920479c0551e6dad09a3a8b8b1
pavelshm10 <pavelsh00@gmail.com>
2024-07-04 10:10:16 +0300

    test

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ app.use('/api', productRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server123 is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
