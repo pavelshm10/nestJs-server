33ea9e55bea9a938bfe683c814d75ce6044464e9
pavelshm10 <pavelsh00@gmail.com>
2024-07-06 00:08:53 +0300

    update

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api', productRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
