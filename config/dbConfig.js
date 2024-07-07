688100bdd6ed74b527677d95b8855cc26fd79321
pavelshm10 <pavelsh00@gmail.com>
2024-07-07 18:11:58 +0300

    aaa

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 const mongoDB = 'mongodb+srv://pavelsh00:tYw7Z7elnXO3K9jS@test.4bsjii7.mongodb.net/?retryWrites=true&w=majority&appName=Test';
 
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true, connectTimeoutMS: 30000 });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
