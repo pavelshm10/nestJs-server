fc52fec753b00cb306c2acb58226b59729203f5e
pavelshm10 <pavelsh00@gmail.com>
2024-07-09 17:04:28 +0300

    up

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -12,7 +12,6 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', function() {
     console.log('MongoDB connected successfully123');
-    testConnection();
 });
 
 module.exports = mongoose;
