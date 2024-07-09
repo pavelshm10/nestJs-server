2614cbc81a82a15990d2c665bdbf4b75edab5bf2
pavelshm10 <pavelsh00@gmail.com>
2024-07-09 17:01:42 +0300

    success

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -5,9 +5,7 @@ const mongoDB = 'mongodb+srv://pavelsh00:tYw7Z7elnXO3K9jS@test123.4bsjii7.mongod
 mongoose.connect(mongoDB, { 
     useNewUrlParser: true, 
     useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 10000, // 30 seconds
-    // socketTimeoutMS: 45000, // 45 seconds
-    // family: 4 // Use IPv4, skip trying IPv6
+    serverSelectionTimeoutMS: 10000,
 });
 
 const db = mongoose.connection;
@@ -16,32 +14,6 @@ db.once('open', function() {
     console.log('MongoDB connected successfully123');
     testConnection();
 });
-
-async function testConnection() {
-    try {
-        // Assuming you have a collection named 'test' in your database
-        const collection = db.collection('test');
-        const result = await collection.findOne({});
-        console.log('Test query result:', result);
-    } catch (err) {
-        console.error('Error running test query:', err);
-    } finally {
-        // Close the connection after the test query
-        mongoose.connection.close();
-    }
-}
-
-// Test a simple query
-// const testConnection = async () => {
-//     try {
-//       const result = await db.admin().ping();
-//       console.log('Ping result:', result);
-//     } catch (err) {
-//       console.error('Error pinging MongoDB:', err);
-//     }
-//   };
-  
-//   testConnection();
 
 module.exports = mongoose;
 
