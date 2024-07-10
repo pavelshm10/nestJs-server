abf7fa394a9269f05e63bd2ead0c40d93f7b81bc
pavelshm10 <pavelsh00@gmail.com>
2024-07-10 10:02:09 +0300

    https

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const productRoutes = require('./routes/productRoutes');
-const mongoose = require('./config/dbConfig');
-// const corsOptions = {
-//   origin: 'https://pavelshm10.github.io/nestjs/', // Replace with your GitHub Pages URL
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// };
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const productRoutes = require("./routes/productRoutes");
+const https = require("https");
+const fs = require('fs');
+const mongoose = require("./config/dbConfig");
 const corsOptions = {
-  origin: '*', // Update with specific origin for production
-  optionsSuccessStatus: 200
+  origin: "https://pavelshm10.github.io/nestjs/", // Replace with your GitHub Pages URL
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
+// const corsOptions = {
+//   origin: '*', // Update with specific origin for production
+//   optionsSuccessStatus: 200
+// };
 const app = express();
 app.use(cors(corsOptions));
 // app.options('/api/products', cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/api', productRoutes);
+app.use("/api", productRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on port ${PORT}`);
+const options = {
+  key: fs.readFileSync("server.key"),
+  cert: fs.readFileSync("server.cert"),
+};
+
+https.createServer(options, app).listen(3000, () => {
+  console.log("HTTPS Server running on port 3000");
 });
