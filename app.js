7fe803dc68b0898bf19d22bb6c265321a8b71878
pavel <pavelsh00@gmail.com>
2024-07-17 15:03:43 +0300

    crt

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use("/api", productRoutes);
 
 const options = {
   key: fs.readFileSync("server.key"),
-  cert: fs.readFileSync("server.cert"),
+  cert: fs.readFileSync("server.crt"),
 };
 
 https.createServer(options, app).listen(3000, () => {
