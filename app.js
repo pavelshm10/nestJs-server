42d9d913e504df3dd4ae393c47ec433b506f6c71
pavel <pavelsh00@gmail.com>
2024-07-18 18:47:04 +0300

    aa

--- app.js
+++ app.js
@@ -1,42 +1,38 @@
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const productRoutes = require("./routes/productRoutes");
-const https = require("https");
-const fs = require('fs');
+// const https = require("https");
+// const fs = require('fs');
 const mongoose = require("./config/dbConfig");
-const agent = new https.Agent({
-  rejectUnauthorized: false, // Ignore SSL certificate errors
-});
-const app = express();
-app.use(cors({
-  origin: 'https://main.d3rbcp5dx3dvou.amplifyapp.com/', // Allow all origins
-  optionsSuccessStatus: 200,
-  rejectUnauthorized: false
-  // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-}));
 
-// app.use(express.json());
+const app = express();
+
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+  // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+
+app.use(express.json());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api", productRoutes);
 
-const options = {
-  // key: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem'),
-  // cert: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/fullchain.pem'),
-  key: fs.readFileSync("server.key"),
-  cert: fs.readFileSync("server.cert"),
-};
+// const options = {
+//   // key: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem'),
+//   // cert: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/fullchain.pem'),
+//   key: fs.readFileSync("server.key"),
+//   cert: fs.readFileSync("server.cert"),
+// };
 
-const server = https.createServer(options, app);
-
-const PORT = process.env.PORT || 443;
-
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log(`Server running on http://localhost:3000`);
 });
-
-// https.createServer(options, app).listen(3000, () => {
-//   console.log("HTTPS Server running on port 3000");
-// });
