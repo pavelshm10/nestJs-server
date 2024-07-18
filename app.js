845413f79a4ec168f973c7388febc935f8a49a70
pavel <pavelsh00@gmail.com>
2024-07-18 23:56:49 +0300

    asd

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const productRoutes = require("./routes/productRoutes");
-// const https = require("https");
-// const fs = require('fs');
+const https = require("https");
+const fs = require('fs');
 const mongoose = require("./config/dbConfig");
 
 const app = express();
@@ -13,7 +13,7 @@ const app = express();
 const corsOptions = {
   origin: '*',
   methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type'],
+  // allowedHeaders: ['Content-Type'],
   // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
@@ -26,13 +26,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api", productRoutes);
 
-// const options = {
-//   // key: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem'),
-//   // cert: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/fullchain.pem'),
-//   key: fs.readFileSync("server.key"),
-//   cert: fs.readFileSync("server.cert"),
-// };
+const options = {
+  key: fs.readFileSync("domain.key"),
+  cert: fs.readFileSync("domain.crt"),
+};
 
-app.listen(3000, () => {
-  console.log(`Server running on http://localhost:3000`);
+https.createServer(options, app).listen(3000, () => {
+  console.log('Server running on https://localhost:3000/');
 });
